7d16cce9b0bbde2106af95a0be18ca1cffb3bb8f
JoGi <girschik@qabel.de>
2017-01-19 13:01:17 +0100

    remove unused extensions

--- client/src/main/java/de/qabel/client/box/extensions.kt
+++
@@ -1,9 +0,0 @@
-package de.qabel.client.box
-
-import de.qabel.client.box.interactor.BrowserEntry
-import de.qabel.client.box.interactor.DownloadSource
-import java.io.ByteArrayInputStream
-
-fun ByteArray.toDownloadSource(entry: BrowserEntry.File)
-    = DownloadSource(entry, ByteArrayInputStream(this))
-
